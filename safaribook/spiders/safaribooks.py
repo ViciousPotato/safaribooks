1337c7f93286a0bb28bea398e3427628ce688cf9
Zhou Honglin <Zhou.Honglin@gmail.com>
2016-09-18 01:29:20 +0800

    Initial commit

---
+++ safaribook/spiders/safaribooks.py
@@ -0,0 +1,97 @@
+import os
+import re
+import ast
+import shutil
+from functools import partial
+import codecs
+
+import scrapy
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+from scrapy.shell import inspect_response
+from jinja2 import Template
+
+null = None
+false = False
+
+PAGE_TEMPLATE="""
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+{{body}}
+</body>
+</html>"""
+
+class SafariBooksSpider(scrapy.Spider):
+  toc_url = 'https://www.safaribooksonline.com/nest/epub/toc/?book_id='
+  name = "SafariBooks"
+  #allowed_domains = []
+  start_urls = ["https://www.safaribooksonline.com/"]
+  host = "https://www.safaribooksonline.com/"
+
+  def __init__(self, user='', password='', bookid=''):
+    self.user = user
+    self.password = password
+    self.bookid = bookid
+    self.info = {}
+    self.initialize_output()
+
+  def initialize_output(self):
+    shutil.rmtree('output/')
+    shutil.copytree('data/', 'output/')
+
+  def parse(self, response):
+    return scrapy.FormRequest.from_response(
+      response,
+      formdata={"email": self.user, "password1": self.password},
+      callback=self.after_login)
+
+  def after_login(self, response):
+    if not 'Recommended For You' in response.body:
+      self.logger.error("Failed login")
+      return
+    yield scrapy.Request(self.toc_url+self.bookid, callback=self.parse_toc)
+
+  def parse_cover_img(self, name, response):
+    #inspect_response(response, self)
+    with open("./output/OEBPS/cover-image.jpg", "w") as f:
+      f.write(response.body)
+
+  def parse_content_img(self, img, response):
+    img_path = os.path.join("./output/OEBPS", img)
+ 
+    img_dir = os.path.dirname(img_path)
+    if not os.path.exists(img_dir):
+      os.makedirs(img_dir)
+
+    with open(img_path, "wb") as f:
+      f.write(response.body)
+
+  def parse_page_json(self, title, bookid, response):
+    page_json = eval(response.body)
+    self.logger.info("Got response: ", page_json)
+    yield scrapy.Request(page_json["content"], callback=partial(self.parse_page, title, bookid, page_json["full_path"]))
+
+  def parse_page(self, title, bookid, path, response):
+    template = Template(PAGE_TEMPLATE)
+    with codecs.open("./output/OEBPS/" + path, "wb", "utf-8") as f:
+      f.write(template.render(body=response.body.decode('utf8')))
+
+    for img in response.xpath("//img/@src").extract():
+      if img:
+        yield scrapy.Request(self.host + '/library/view/' + title + '/' + bookid + '/' + img,
+                             callback=partial(self.parse_content_img, img))
+
+  def parse_toc(self, response):
+    self.logger.info("Got toc")
+    toc = eval(response.body)
+    cover_path, = re.match(r'<img src="(.*?)" alt.+', toc["thumbnail_tag"]).groups()
+    yield scrapy.Request(self.host + cover_path,
+                         callback=partial(self.parse_cover_img, "cover-image"))
+    for item in toc["items"]:
+      yield scrapy.Request(self.host + item["url"], callback=partial(self.parse_page_json, toc["title_safe"], toc["book_id"]))
+
+    template = Template(file("./output/OEBPS/content.opf").read())
+    with codecs.open("./output/OEBPS/content.opf", "wb", "utf-8") as f:
+      f.write(template.render(info=toc))
