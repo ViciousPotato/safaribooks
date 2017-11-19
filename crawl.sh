63157f2eb817f112eaf4b4d6c5dd1ee1a0b4c3f3
Zhou Honglin <zhou.honglin@gmail.com>
2017-11-19 12:18:16 +0800

    Add book id format checking to avoid case of #13

--- crawl.sh
+++ crawl.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+bookid_regex="\d+"
+
+if ! [[ $3 =~ \d+ ]]; then
+    echo "Please give a pure digit book id, $3 is not an acceptable book id."
+    exit -1
+fi
+
 scrapy crawl SafariBooks -a user=$1 -a password=$2 -a bookid=$3
 kindlegen *.epub
